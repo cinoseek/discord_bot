295fb63a239e9b9c62a709034fe1f0a9f6080e81
cno <cinoseek@gmail.com>
2018-01-30 15:43:09 +0900

    1. react self !선택 command 2. Add !타이머 function

--- start.js
+++ start.js
@@ -37,13 +37,18 @@ function make_alias_list() {
 }
 make_alias_list();
 
+// Timer func
+function timer_msg(message, caller , time, msg) {
+    message.channel.sendMessage(caller + ' ' + time + '초 타이머 종료: ' + msg);
+}
+
 client.on('ready',() => {
     console.log('Online');
 });
 
 client.on('message', message => {
     try {
-        if (message.author === client.user) return;
+        // if (message.author === client.user) return;
 
         // run only prefix msg
         var msg = message.content;
@@ -65,7 +70,7 @@ client.on('message', message => {
 
         // Print All
         if (msg.startsWith(prefix + '도움')) {
-            all = '';
+            all = '명령어 목록: ';
             for ( i in alias_list) {
                 all = all + i + ' ';
             }
@@ -81,7 +86,26 @@ client.on('message', message => {
 
         if (msg.startsWith(prefix + '선택')) {
             items = (msg.replace(msg.split(' ')[0],'').trim()).split(' ');
-            message.channel.sendMessage(message.author.username + ' 선택: ' + items[math.floor(math.random() * items.length)]);
+            //message.channel.sendMessage(message.author.username + ' 선택: ' + items[math.floor(math.random() * items.length)]);
+            retmsg = items[math.floor(math.random() * items.length)]
+            if (0 != retmsg.length)
+                message.channel.sendMessage(retmsg);
+            return;
+        }
+
+        // Timer
+        // !타이머 1800 모닥불
+        if (msg.startsWith(prefix + '타이머')) {
+            items = (msg.replace(msg.split(' ')[0],'').trim()).split(' ');
+            if (0 == items) {
+                message.channel.sendMessage('ex) !타이머 1700초 모닥불');
+            }
+            else {
+                time = parseInt(items[0]);
+                msg  = items[1];
+                message.channel.sendMessage(message.author.toString() + ' ' + time + '초 ' + msg + ' 타이머 시작');
+                setTimeout(timer_msg, time*1000, message, message.author.toString(), time, msg);
+            }
             return;
         }
 
