dbcf67df15ee514f2e90ddd6b362eddf6c7e0e73
cno <cinoseek@gmail.com>
2018-06-25 20:15:01 +0900

    Type practice

--- start.js
+++ start.js
@@ -11,7 +11,17 @@ const math = require('mathjs');
 var doc = new GoogleSpreadsheet(settings.alias_sheet_id);
 var alias_list = [];
 var lucky_list = [];
-var prefix = '!'
+var type_list  = [];
+var type_num   = [];
+var prefix = '!';
+
+// Type practice
+var type_array = [];
+var typeon = false;
+var type_stime;
+var type_etime;
+var typemsg;
+var tnum;
 
 function make_alias_list() {
     /*
@@ -21,6 +31,8 @@ function make_alias_list() {
     */
     alias_list = [];
     lucky_list = [];
+    type_list  = [];
+    type_num   = [];
 
     // Authenticate with the Google Spreadsheets API.
     doc.useServiceAccountAuth(creds, function (err) {
@@ -45,6 +57,17 @@ function make_alias_list() {
             }
             // console.log(alias_list);
         });
+        doc.getRows(3,{
+            offset: 1,
+            limit: 1000,
+        }, function (err, rows) {
+            console.log('Read type '+rows.length+' rows');
+            for ( i in rows ) {
+                type_list.push(rows[i].type);
+                type_num.push(rows[i].num);
+            }
+            // console.log(alias_list);
+        });
     });
 }
 make_alias_list();
@@ -52,6 +75,28 @@ make_alias_list();
 // Timer func
 function timer_msg(message, caller , time, msg) {
     message.channel.sendMessage(caller + ' ' + time + '초 타이머 종료: ' + msg);
+}
+// Type timer
+function timer_type_start(message) {
+    typeon = true;
+    message.channel.sendMessage('제시문: '+ typemsg);
+    type_stime = Date.now();
+}
+function timer_type_wait(message) {
+    typeon = false;
+    function type_sort(callback) {
+        type_array.sort(function(a, b) {
+            return a[1] - b[1];
+        });
+        callback();
+    }
+    type_sort(function() {
+        var result="타자연습 결과\n";
+        for (i=0;i<type_array.length;i++) {
+            result += type_array[i][1] + "타/분 " + type_array[i][0] + '\n';
+        }
+        message.channel.sendMessage(result)
+    });
 }
 
 client.on('ready',() => {
@@ -64,7 +109,7 @@ client.on('message', message => {
 
         // run only prefix msg
         var msg = message.content;
-        if (!msg.startsWith(prefix)) return;
+        if (!typeon && !msg.startsWith(prefix)) return;
 
         // test ping
         if (msg.startsWith(prefix + 'ping')) {
@@ -128,6 +173,27 @@ client.on('message', message => {
             console.log('lucky num: '+pick+'/'+lucky_list.length);
             if (0 != retmsg.length)
                 message.channel.sendMessage(retmsg);
+        }
+
+        // type test
+        if (!typeon && msg.startsWith(prefix + '타자연습')) {
+            pick1 = math.floor(math.random() * type_list.length);
+            pick2 = math.floor(math.random() * type_list.length);
+            typemsg = type_list[pick1] + ' ' + type_list[pick2];
+            tnum = Number(type_num[pick1]) + Number(type_num[pick2]) + 1;
+            message.channel.sendMessage("5초후 제시문을 타이핑하세요.");
+            type_arry = [];
+            console.log('type msg: ' + typemsg);
+            setTimeout(timer_type_start, 5*1000, message);
+            setTimeout(timer_type_wait, 20*1000, message);
+        }
+        if (msg.startsWith(typemsg)) {
+            type_etime = Date.now();
+            interval = (type_etime - type_stime)/1000.0;
+            console.log('interval:' + interval);
+            speed = Number((tnum*60.0/interval).toFixed(1));
+            console.log('speed:' + speed);
+            type_array.push([message.author.toString(), speed]);
         }
 
         // print google sheet items
