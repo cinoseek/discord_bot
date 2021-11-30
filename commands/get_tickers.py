ad5e13a28c8e6ac0a1fee9e1ed40c5b9c75a7769
cno <cinoseek@gmail.com>
2021-12-01 00:14:07 +0900

    Update get_tickers python function for ETF

--- commands/get_tickers.py
+++ commands/get_tickers.py
@@ -1,6 +1,10 @@
 from pykrx import stock
 import pandas as pd
 
-tickers = stock.get_market_ticker_list(market="ALL")
-df = pd.DataFrame([[ticker,stock.get_market_ticker_name(ticker)] for ticker in tickers], columns = ["ticker", "name"])
-df.to_csv('tickers.csv')
+all_tickers = stock.get_market_ticker_list(market="ALL")
+etf_tickers = stock.get_etf_ticker_list()
+df_all = pd.DataFrame([[ticker,stock.get_market_ticker_name(ticker)] for ticker in all_tickers], columns = ["ticker", "name"])
+df_etf = pd.DataFrame([[ticker,stock.get_etf_ticker_name(ticker)] for ticker in etf_tickers], columns = ["ticker", "name"])
+
+df_all = df_all.append(df_etf)
+df_all.to_csv('tickers.csv')
